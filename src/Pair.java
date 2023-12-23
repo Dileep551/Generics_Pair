5b7382fbb93aa382f1ffc755486f303a9bfa57f0
e146508 <Dileep.Kessireddy@wnco.com>
2023-12-23 09:21:29 -0600

    Added customer record, Just to have different type on Pair & added test case for it.

--- src/Pair.java
+++ src/Pair.java
@@ -1,6 +1,8 @@
 
 record Person(String givenName, String surname) {};
 
+record Customer(String givenName, String surname) {};
+
 public record Pair<T, U>(T first, U second){
 
     public static <T, U> Pair<T, U> of(T first, U second){
--- src/test/PairTest.java
+++ src/test/PairTest.java
@@ -9,27 +9,21 @@ class PairTest {
 
         //Given
         Person person = new Person("Dileep", "Reddy");
-        Pair<String, Person> pair = Pair.of("e146508", person);
+        Pair<String, Person> pair1 = Pair.of("e146508", person);
+
+        Customer customer = new Customer("John", "Wick");
+        Pair<Integer, Customer> pair2 = Pair.of(146508, customer);
 
 
         //Then
-        assertEquals("e146508", pair.first());
-        assertEquals(person, pair.second());
+        assertEquals("e146508", pair1.first());
+        assertEquals(person, pair1.second());
+
+        assertEquals(146508, pair2.first());
+        assertEquals(customer, pair2.second());
 
     }
 
-
-    @Test
-    void testPersonRecord() {
-
-        //Given
-        Person person = new Person("Dileep", "Reddy");
-
-        //Then
-        assertEquals("Dileep", person.givenName());
-        assertEquals("Reddy", person.surname());
-
-    }
 
     @Test
     void testPairEquality(){
@@ -41,9 +35,13 @@ class PairTest {
         Person person2 = new Person("Dileep", "Reddy");
         Pair<String, Person> pair2 = Pair.of("e146508", person2);
 
+        Customer customer = new Customer("John", "Wick");
+        Pair<Integer, Customer> pair3 = Pair.of(146508, customer);
+
 
         //Then
         assertEquals(pair1, pair2);
+        assertNotEquals(pair1, pair3);
 
     }
 
